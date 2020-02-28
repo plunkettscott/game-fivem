d64b482582d76fa5dead4657fdb1f3c82d532993
Xander Harrison <xander.harrison77@gmail.com>
2020-02-27 21:58:24 -0500

    Initial Commit

---
+++ response/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+game 'gta5'
+
+name 'Response'
+description 'Base resource for Response'
+author 'Aeoss Technology'
+url 'RESPONSE_URL_HERE'
+
+client_script 'dist/client.js'
+server_script 'dist/server.js'
---
+++ response_livemap/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+game 'gta5'
+
+name 'Response Live Map'
+description 'Live Map resource for Response'
+author 'Aeoss Technology'
+url 'RESPONSE_URL_HERE'
+
+client_script 'dist/client.js'
+server_script 'dist/server.js'
